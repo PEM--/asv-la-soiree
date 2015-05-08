35a65345b5a03d73dbf97123aba7e4e7d91656e1
PEM-- <pemarchandet@gmail.com>
2015-05-09 01:46:49 +0200

    Internationalization support

---
+++ app/server/mfAll.js
@@ -0,0 +1,51 @@
+mfPkg.syncAll({
+  "en": {
+    "home": {
+      "key": "home",
+      "lang": "en",
+      "text": "Home",
+      "ctime": "2015-05-08T23:41:07.000Z",
+      "mtime": "2015-05-08T23:41:07.000Z",
+      "revisionId": "AYbR3HFvxkGd5NBqc",
+      "file": "client/views/home/home.jade",
+      "line": 2,
+      "template": "home",
+      "_id": "hAvqAhMq2dDmLDGmQ"
+    },
+    "welcome": {
+      "key": "welcome",
+      "lang": "en",
+      "text": "Welcome",
+      "ctime": "2015-05-08T23:41:07.000Z",
+      "mtime": "2015-05-08T23:41:07.000Z",
+      "revisionId": "dXXgF5mWHtWErgz2Y",
+      "file": "client/views/home/home.jade",
+      "line": 3,
+      "template": "home",
+      "_id": "2odhjPEE7cWKJ9K4q"
+    }
+  },
+  "fr": {
+    "home": {
+      "key": "home",
+      "lang": "fr",
+      "text": "Accueil",
+      "ctime": 1431128507983,
+      "mtime": 1431128507983,
+      "revisionId": "GxxBg5SdePZjur6wN",
+      "_id": "cF5riYu7iY9iRffMf"
+    },
+    "welcome": {
+      "key": "welcome",
+      "lang": "fr",
+      "text": "Bienvenu",
+      "ctime": 1431128517330,
+      "mtime": 1431128517330,
+      "revisionId": "YoarZxh2caJpYwwAu",
+      "_id": "Bsf9ZistujxYj6HEJ"
+    }
+  }
+}, {
+  "exportedAt": 1431128523823,
+  "updatedAt": 1431128517330
+});
