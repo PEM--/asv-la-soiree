24b9712360548c60a1fc003734f75b2ab90ceaef
PEM-- <pemarchandet@gmail.com>
2015-05-09 12:12:45 +0200

    Internationalization support

--- app/server/mfAll.js
+++ app/server/mfAll.js
@@ -1,51 +1,300 @@
 mfPkg.syncAll({
   "en": {
-    "home": {
-      "key": "home",
+    "invalid_email": {
+      "key": "invalid_email",
       "lang": "en",
-      "text": "Home",
-      "ctime": "2015-05-08T23:41:07.000Z",
-      "mtime": "2015-05-08T23:41:07.000Z",
-      "revisionId": "AYbR3HFvxkGd5NBqc",
+      "text": "The email is not valid",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "q9yYdkLLBLaFevtoe",
       "file": "client/views/home/home.jade",
-      "line": 2,
+      "line": 4,
       "template": "home",
-      "_id": "hAvqAhMq2dDmLDGmQ"
+      "_id": "5GsLzma4ACipALFhH"
     },
-    "welcome": {
-      "key": "welcome",
+    "passwords_must_match": {
+      "key": "passwords_must_match",
       "lang": "en",
-      "text": "Welcome",
-      "ctime": "2015-05-08T23:41:07.000Z",
-      "mtime": "2015-05-08T23:41:07.000Z",
-      "revisionId": "dXXgF5mWHtWErgz2Y",
+      "text": "Passwords must match",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "2Tqx7gB6jttLEqaEv",
       "file": "client/views/home/home.jade",
-      "line": 3,
+      "line": 5,
       "template": "home",
-      "_id": "2odhjPEE7cWKJ9K4q"
+      "_id": "i8daMkiuexdezJP5S"
+    },
+    "no_permission_for_roles": {
+      "key": "no_permission_for_roles",
+      "lang": "en",
+      "text": "You have no permissions to change user roles",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "NJkzWsWjutprkG2ZA",
+      "file": "client/views/home/home.jade",
+      "line": 6,
+      "template": "home",
+      "_id": "oJGfZYam9wXjbHAzh"
+    },
+    "invalid_invitation_code": {
+      "key": "invalid_invitation_code",
+      "lang": "en",
+      "text": "The invitation code is invalid",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "x7wxL7HqC5m3q8648",
+      "file": "client/views/home/home.jade",
+      "line": 7,
+      "template": "home",
+      "_id": "8SPBK9jbDn2b3jLks"
+    },
+    "my_account": {
+      "key": "my_account",
+      "lang": "en",
+      "text": "My Account",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "YYs8uDH22AtddNrfJ",
+      "file": "client/views/home/home.jade",
+      "line": 8,
+      "template": "home",
+      "_id": "LzcPyD6RMrHWQF7yc"
+    },
+    "name": {
+      "key": "name",
+      "lang": "en",
+      "text": "Name",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "tHkyQb8SkwxDBSyhL",
+      "file": "client/views/home/home.jade",
+      "line": 9,
+      "template": "home",
+      "_id": "wofgo76ZcTHSrN3ki"
+    },
+    "your_name": {
+      "key": "your_name",
+      "lang": "en",
+      "text": "Your name",
+      "ctime": "2015-05-09T09:46:46.000Z",
+      "mtime": "2015-05-09T09:46:46.000Z",
+      "revisionId": "92rfZTjCLihcBgEJw",
+      "file": "client/views/home/home.jade",
+      "line": 10,
+      "template": "home",
+      "_id": "b2AM69K9neDweWNXd"
+    },
+    "email": {
+      "key": "email",
+      "lang": "en",
+      "text": "Email",
+      "ctime": "2015-05-09T10:05:45.000Z",
+      "mtime": "2015-05-09T10:05:45.000Z",
+      "revisionId": "8A9JQ9Z97pGNNHPtv",
+      "file": "client/views/home/home.jade",
+      "line": 11,
+      "template": "home",
+      "_id": "aYcjrEPdWawN6TJpj"
+    },
+    "roles": {
+      "key": "roles",
+      "lang": "en",
+      "text": "Roles",
+      "ctime": "2015-05-09T10:05:45.000Z",
+      "mtime": "2015-05-09T10:05:45.000Z",
+      "revisionId": "cdevQRwFozdrPZ7Q7",
+      "file": "client/views/home/home.jade",
+      "line": 12,
+      "template": "home",
+      "_id": "wg64Lbi5tb7esQBxE"
+    },
+    "actions": {
+      "key": "actions",
+      "lang": "en",
+      "text": "Actions",
+      "ctime": "2015-05-09T10:05:45.000Z",
+      "mtime": "2015-05-09T10:05:45.000Z",
+      "revisionId": "9gCPcX9LD8ykaTQP8",
+      "file": "client/views/home/home.jade",
+      "line": 13,
+      "template": "home",
+      "_id": "45JXvxRtp55N8Lq85"
+    },
+    "logout": {
+      "key": "logout",
+      "lang": "en",
+      "text": "Logout",
+      "ctime": "2015-05-09T10:05:45.000Z",
+      "mtime": "2015-05-09T10:05:45.000Z",
+      "revisionId": "4e7en7XhsgybxgobK",
+      "file": "client/views/home/home.jade",
+      "line": 14,
+      "template": "home",
+      "_id": "Gi5dCgyErF6jtzukB"
+    },
+    "update_profile": {
+      "key": "update_profile",
+      "lang": "en",
+      "text": "Update Profile",
+      "ctime": "2015-05-09T10:05:45.000Z",
+      "mtime": "2015-05-09T10:05:45.000Z",
+      "revisionId": "CQ2xq9hd5vzAirNRK",
+      "file": "client/views/home/home.jade",
+      "line": 15,
+      "template": "home",
+      "_id": "fArtS4nFSzWHyzLK4"
+    },
+    "change_password": {
+      "key": "change_password",
+      "lang": "en",
+      "text": "Change Password",
+      "ctime": "2015-05-09T10:05:45.000Z",
+      "mtime": "2015-05-09T10:05:45.000Z",
+      "revisionId": "fL9eDedBij8eMEP4c",
+      "file": "client/views/home/home.jade",
+      "line": 16,
+      "template": "home",
+      "_id": "62XqvuiJ3fZkeELpP"
     }
   },
   "fr": {
-    "home": {
-      "key": "home",
+    "accounts": {
+      "key": "accounts",
       "lang": "fr",
-      "text": "Accueil",
-      "ctime": 1431128507983,
-      "mtime": 1431128507983,
-      "revisionId": "GxxBg5SdePZjur6wN",
-      "_id": "cF5riYu7iY9iRffMf"
+      "text": "Comptes",
+      "ctime": 1431161486814,
+      "mtime": 1431161486814,
+      "revisionId": "GarSF6HcCaTDEm26f",
+      "_id": "xojR6YMDmXn3ifmEo"
     },
-    "welcome": {
-      "key": "welcome",
+    "invalid_email": {
+      "key": "invalid_email",
       "lang": "fr",
-      "text": "Bienvenu",
-      "ctime": 1431128517330,
-      "mtime": 1431128517330,
-      "revisionId": "YoarZxh2caJpYwwAu",
-      "_id": "Bsf9ZistujxYj6HEJ"
+      "text": "L'email n'est pas valide",
+      "ctime": 1431162539451,
+      "mtime": 1431162539451,
+      "revisionId": "N5eskjRAj47nGdFx9",
+      "_id": "dmzMRt2f7BPZEnDdG"
+    },
+    "passwords_must_match": {
+      "key": "passwords_must_match",
+      "lang": "fr",
+      "text": "Les mots de passes doivent correspondre",
+      "ctime": 1431162559188,
+      "mtime": 1431162559189,
+      "revisionId": "WFYCxaTwPwzmF9Zi2",
+      "_id": "E8CcRmuWXs44YwMqW"
+    },
+    "edit_roles": {
+      "key": "edit_roles",
+      "lang": "fr",
+      "text": "Editer les rôles",
+      "ctime": 1431163702521,
+      "mtime": 1431163702521,
+      "revisionId": "QoaP3ZFd5DdFSpXJy",
+      "_id": "CdC6EaNpBtLbDSfn5"
+    },
+    "no_permission_for_roles": {
+      "key": "no_permission_for_roles",
+      "lang": "fr",
+      "text": "Vous n'avez pas la permission de changer les rôles utilisateurs",
+      "ctime": 1431163727177,
+      "mtime": 1431163727177,
+      "revisionId": "XfdjmPzrpBwoPMkc9",
+      "_id": "4BMH6HwRyDQkJniJp"
+    },
+    "invalid_invitation_code": {
+      "key": "invalid_invitation_code",
+      "lang": "fr",
+      "text": "Le code d'invitation n'est pas valide",
+      "ctime": 1431163737632,
+      "mtime": 1431163737632,
+      "revisionId": "YKb4zzCwPzskjDJYQ",
+      "_id": "xdfg88vcNadSnYg8b"
+    },
+    "my_account": {
+      "key": "my_account",
+      "lang": "fr",
+      "text": "Mon compte",
+      "ctime": 1431163742265,
+      "mtime": 1431163742265,
+      "revisionId": "Jq2ejQyxgoP2kqPJM",
+      "_id": "dm7rYR9HpN6Hqjg4f"
+    },
+    "name": {
+      "key": "name",
+      "lang": "fr",
+      "text": "Nom",
+      "ctime": 1431164837335,
+      "mtime": 1431164837335,
+      "revisionId": "TN4W9bou9tsjP7jRW",
+      "_id": "CpXJ6grvJZfef8q2h"
+    },
+    "your_name": {
+      "key": "your_name",
+      "lang": "fr",
+      "text": "Votre nom",
+      "ctime": 1431164856854,
+      "mtime": 1431164856854,
+      "revisionId": "285xJcdyrzbyKog2v",
+      "_id": "kE8BQN3SHrut5TxoY"
+    },
+    "email": {
+      "key": "email",
+      "lang": "fr",
+      "text": "Email",
+      "ctime": 1431165980509,
+      "mtime": 1431165980509,
+      "revisionId": "k6qFrWhmHhodbeubY",
+      "_id": "C8Hz4aR8Ei8AfxAfA"
+    },
+    "roles": {
+      "key": "roles",
+      "lang": "fr",
+      "text": "Rôles",
+      "ctime": 1431165984226,
+      "mtime": 1431165984226,
+      "revisionId": "kR9RWsGaYi9tmTaoa",
+      "_id": "5TdwBmPnLbQP6GuXj"
+    },
+    "actions": {
+      "key": "actions",
+      "lang": "fr",
+      "text": "Actions",
+      "ctime": 1431165987131,
+      "mtime": 1431165987131,
+      "revisionId": "4HXsEWrcLqx53CvtH",
+      "_id": "cMyYMZEayiK9Znb5h"
+    },
+    "logout": {
+      "key": "logout",
+      "lang": "fr",
+      "text": "Déconnexion",
+      "ctime": 1431165993852,
+      "mtime": 1431165993852,
+      "revisionId": "AMvohxo6rqSBDay9z",
+      "_id": "xNQMQ9PgSLyiDgLsy"
+    },
+    "update_profile": {
+      "key": "update_profile",
+      "lang": "fr",
+      "text": "Mettre le profile à jour",
+      "ctime": 1431166021019,
+      "mtime": 1431166021019,
+      "revisionId": "v7L9PCLeJbG6CrKmc",
+      "_id": "WPLMwAesQ73qDiih8"
+    },
+    "change_password": {
+      "key": "change_password",
+      "lang": "fr",
+      "text": "Changer le mot de passe",
+      "ctime": 1431166027850,
+      "mtime": 1431166027850,
+      "revisionId": "sirgvRnnA5pEgagCW",
+      "_id": "uHsafxvSHmgNj9zH7"
     }
   }
 }, {
-  "exportedAt": 1431128523823,
-  "updatedAt": 1431128517330
+  "exportedAt": 1431166049812,
+  "updatedAt": 1431166027850
 });
