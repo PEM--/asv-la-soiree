f96c02012bbc007e189655d2dcf2ff99f52de7b9
PEM-- <pemarchandet@gmail.com>
2015-09-01 22:31:11 +0200

    Docker - Removing settings.json - pending

--- docker/buildMeteor.sh
+++ docker/buildMeteor.sh
@@ -3,5 +3,4 @@ rm -rf meteor/bundle nginx/bundle
 cd ../app
 meteor build --architecture os.linux.x86_64 --directory ../docker/meteor
 cd -
-cp ../app/production.json meteor/bundle/settings.json
 cp -R meteor/bundle nginx
